9b2c244fafb0d412eddb97cb3d031cf8c48396a5
Alexander Plavin <alexander@plav.in>
2014-07-24 14:51:27 +0400

    python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ipy-progressbar',
+      version='0.1',
+      description='Progressbar for both IPython Notebooks (HTML, CSS, JS) and plain terminals.',
+      url='https://hg.plav.in/my_projects/ipy-progressbar',
+      author='Alexander Plavin',
+      author_email='alexander@plav.in',
+      license='MIT',
+      packages=['ipy-progressbar'],
+      zip_safe=False)
