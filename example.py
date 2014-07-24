f51b74a7bd8f699804c59ff67b14c45f6e26ca6b
Alexander Plavin <alexander@plav.in>
2014-07-24 23:58:37 +0400

    modify examples

--- example.py
+++ example.py
@@ -1,8 +1,21 @@
 from ipy_progressbar.terminal_bar import ProgressBarTerminal
+from ipy_progressbar import ProgressBar
 from time import sleep
+from random import random
 
-pb = ProgressBarTerminal(80, title='Outer')
+# both should work:
+
+pb = ProgressBarTerminal(5, title='Outer', key='outer')
 for i in pb:
-    pb_inner = ProgressBarTerminal(80, title='Inner')
+    pb_inner = ProgressBarTerminal(5, title='Inner', key='inner')
     for j in pb_inner:
-        sleep(0.05)
+        sleep(0.5 * random())
+        # pb.set_extra_text('inner: %d' % j)
+
+
+pb = ProgressBar(5, title='Outer', key='outer')
+for i in pb:
+    pb_inner = ProgressBar(5, title='Inner', key='inner')
+    for j in pb_inner:
+        sleep(0.5 * random())
+        # pb.set_extra_text('inner: %d' % j)
