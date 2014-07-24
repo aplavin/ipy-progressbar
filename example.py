410e8233a2439d3d621dfb57a1c1e04cf0987b0f
Alexander Plavin <alexander@plav.in>
2014-07-24 22:16:42 +0400

    add nested progressbars and title to example.py

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 from ipy_progressbar.terminal_bar import ProgressBarTerminal
 from time import sleep
 
-pb = ProgressBarTerminal(80)
+pb = ProgressBarTerminal(80, title='Outer')
 for i in pb:
-    sleep(0.2)
+    pb_inner = ProgressBarTerminal(80, title='Inner')
+    for j in pb_inner:
+        sleep(0.05)
