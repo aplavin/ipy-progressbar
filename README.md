a63b87831260b3b9b3bbe3c60db51e28ed018851
Alexander Plavin <alexander@plav.in>
2014-07-25 09:58:28 +0400

    add more metadata to setup.py, add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+ipy-progressbar allows to use the same code for rich progressbars in IPython Notebooks, and for simple fallback ones in terminal sessions.
+
+
+Example
+=======
+
+Code like this:
+
+    from ipy_progressbar import ProgressBar
+
+    pb = ProgressBar(10, title='Outer', key='outer')
+    for i in pb:
+        pb_inner = ProgressBar(5, title='Inner', key='inner')
+        for j in pb_inner:
+            # inner loop body
+
+will work both in IPython Notebook and in plain console. When run in notebook, the output will be rich (Bootstrap progress bars), while console version is mainly thought of as a fallback.
--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 from setuptools import setup
 
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = ''
+
 setup(name='ipy-progressbar',
-      version='1.0',
+      version='1.0.1',
       description='Progressbar for both IPython Notebooks (HTML, CSS, JS) and plain terminals.',
+      long_description = long_description,
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Terminals',
+      ],
+      keywords='progress progressbar terminal console ipython notebook',
       url='https://hg.plav.in/my_projects/ipy-progressbar',
       author='Alexander Plavin',
       author_email='alexander@plav.in',
